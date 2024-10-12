75b8b94b868beec6d927715b835be94518004ebd
JanaEsther <j.obendraufova@h2o-logic.cz>
2024-10-12 12:00:00 +0200

    bonus4

--- src/components/ZaverecnyBonus4/index.jsx
+++ src/components/ZaverecnyBonus4/index.jsx
@@ -1,14 +1,28 @@
 import './style.css';
+import { useState } from 'react';
 /*
 Zadání: Pomocí dvou stavových proměnných s výchozí hodnotou 0, které se budou měnit podle vstupních
   políček, vypiš do `<output></output>` jejich součet.
 */
 
 export const ZaverecnyBonus4 = () => {
+  const [soucet1, setSoucet1] = useState(0);
+  const [soucet2, setSoucet2] = useState(0);
+
+  const handleSoucet1Change = (e) => {
+    setSoucet1(Number(e.target.value));
+  };
+  const handleSoucet2Change = (e) => {
+    setSoucet2(Number(e.target.value));
+  };
+
+  const sum = soucet1 + soucet2;
+
   return (
     <div className="bonus4">
-      <input type="number" defaultValue="0" /> +{' '}
-      <input type="number" defaultValue="0" /> = <output>0</output>
+      <input type="number" value={soucet1} onChange={handleSoucet1Change} /> +{' '}
+      <input type="number" value={soucet2} onChange={handleSoucet2Change} /> ={' '}
+      <output>{sum}</output>
     </div>
   );
 };
